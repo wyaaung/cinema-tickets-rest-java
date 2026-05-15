5c1d976b9a19b54721d4cfe71a83d3c0da5a1a90
wyaaung <wya.aung@gmail.com>
2026-05-15 22:20:27 +0100

    feat: implemented TicketRequestAggregator

--- src/main/java/uk/gov/dwp/engineering/recruitment/service/TicketRequestAggregatorImpl.java
+++ src/main/java/uk/gov/dwp/engineering/recruitment/service/TicketRequestAggregatorImpl.java
@@ -1,13 +1,28 @@
 package uk.gov.dwp.engineering.recruitment.service;
 
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 import uk.gov.dwp.engineering.recruitment.domain.TicketRequest;
+import uk.gov.dwp.engineering.recruitment.domain.TicketType;
 
 @Service
 public class TicketRequestAggregatorImpl implements TicketRequestAggregator {
 
   @Override
   public TicketTally aggregate(final TicketRequest... ticketRequests) {
-    return null; // TODO: implement this method
+    final Map<TicketType, Integer> counts = Arrays.stream(ticketRequests)
+        .collect(Collectors.groupingBy(
+            TicketRequest::type,
+            () -> new EnumMap<TicketType, Integer>(TicketType.class),
+            Collectors.summingInt(TicketRequest::ticketCount)));
+
+    return new TicketTally(
+        counts.getOrDefault(TicketType.ADULT, 0),
+        counts.getOrDefault(TicketType.CHILD, 0),
+        counts.getOrDefault(TicketType.INFANT, 0)
+    );
   }
 }
