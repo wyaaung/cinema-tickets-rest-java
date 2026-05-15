581e0154d9ebf0b3ce782cfc5e12ecf26cdeff80
wyaaung <wya.aung@gmail.com>
2026-05-15 22:55:37 +0100

    refactor: iterate TicketType.values() in price calculator implementation

--- src/main/java/uk/gov/dwp/engineering/recruitment/service/TicketPriceCalculatorImpl.java
+++ src/main/java/uk/gov/dwp/engineering/recruitment/service/TicketPriceCalculatorImpl.java
@@ -1,7 +1,10 @@
 package uk.gov.dwp.engineering.recruitment.service;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Map;
 import org.springframework.stereotype.Service;
+import uk.gov.dwp.engineering.recruitment.domain.TicketType;
 
 @Service
 public class TicketPriceCalculatorImpl implements TicketPriceCalculator {
@@ -10,10 +13,15 @@ public class TicketPriceCalculatorImpl implements TicketPriceCalculator {
   private static final BigDecimal CHILD_PRICE = BigDecimal.valueOf(15);
   private static final BigDecimal INFANT_PRICE = BigDecimal.ZERO;
 
+  public static final Map<TicketType, BigDecimal> TYPE_PRICE = Map.of(
+      TicketType.ADULT, ADULT_PRICE,
+      TicketType.CHILD, CHILD_PRICE,
+      TicketType.INFANT, INFANT_PRICE);
+
   @Override
   public BigDecimal totalCost(final TicketTally tally) {
-    return ADULT_PRICE.multiply(BigDecimal.valueOf(tally.adults()))
-        .add(CHILD_PRICE.multiply(BigDecimal.valueOf(tally.children())))
-        .add(INFANT_PRICE.multiply(BigDecimal.valueOf(tally.infants())));
+    return Arrays.stream(TicketType.values())
+        .map(type -> TYPE_PRICE.get(type).multiply(BigDecimal.valueOf(tally.countFor(type))))
+        .reduce(BigDecimal.ZERO, BigDecimal::add);
   }
 }
