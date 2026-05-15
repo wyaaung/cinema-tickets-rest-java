322fd31d95aa259baa85e8659c5577e4f423f0bb
wyaaung <wya.aung@gmail.com>
2026-05-15 22:50:03 +0100

    feat: orchestrated purchase via validator, calculator, payment, and reservation

--- src/main/java/uk/gov/dwp/engineering/recruitment/CinemaTicketsServiceImpl.java
+++ src/main/java/uk/gov/dwp/engineering/recruitment/CinemaTicketsServiceImpl.java
@@ -1,28 +1,51 @@
 package uk.gov.dwp.engineering.recruitment;
 
+import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.UUID;
 import org.springframework.stereotype.Service;
 import uk.gov.dwp.engineering.recruitment.domain.TicketRequest;
 import uk.gov.dwp.engineering.recruitment.exception.InvalidBookingException;
+import uk.gov.dwp.engineering.recruitment.service.TicketPriceCalculator;
+import uk.gov.dwp.engineering.recruitment.service.TicketRequestValidator;
+import uk.gov.dwp.engineering.recruitment.service.TicketTally;
 import uk.gov.dwp.engineering.recruitment.thirdparty.PaymentService;
 import uk.gov.dwp.engineering.recruitment.thirdparty.SeatReservationService;
 
 @Service
 public class CinemaTicketsServiceImpl implements CinemaTicketsService {
 
+  private final TicketRequestValidator validator;
+  private final TicketPriceCalculator priceCalculator;
   private final PaymentService paymentService;
-
   private final SeatReservationService seatReservationService;
 
-  public CinemaTicketsServiceImpl(PaymentService paymentService,
-      SeatReservationService seatReservationService) {
-    this.paymentService = paymentService;
-    this.seatReservationService = seatReservationService;
+  public CinemaTicketsServiceImpl(final TicketRequestValidator validator,
+      final TicketPriceCalculator priceCalculator, final PaymentService paymentService,
+      final SeatReservationService seatReservationService) {
+    this.validator = Objects.requireNonNull(validator, "validator");
+    this.priceCalculator = Objects.requireNonNull(priceCalculator, "priceCalculator");
+    this.paymentService = Objects.requireNonNull(paymentService, "paymentService");
+    this.seatReservationService =
+        Objects.requireNonNull(seatReservationService, "seatReservationService");
   }
 
   @Override
   public String purchaseTickets(final Long accountId, final TicketRequest... ticketRequests)
       throws InvalidBookingException {
 
-    return null;
+    final TicketTally tally = validator.validate(accountId, ticketRequests);
+    final BigDecimal totalCost = priceCalculator.totalCost(tally);
+
+    paymentService.debitAccount(accountId, totalCost);
+    seatReservationService.reserveSeats(accountId, (long) tally.seatCount());
+
+    return buildBookingReference(accountId, totalCost, tally.seatCount());
+  }
+
+  private static String buildBookingReference(final Long accountId, final BigDecimal totalCost,
+      final int seatCount) {
+    return "BOOKING-" + UUID.randomUUID() + "|account=" + accountId + "|cost="
+        + totalCost.toPlainString() + "|seats=" + seatCount;
   }
 }
